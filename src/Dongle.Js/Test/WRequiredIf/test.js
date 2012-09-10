4cff83042ae0500fd051ecceaddae462e6b68583
Afonso <afonso.franca@gmail.com>
2012-09-10 16:21:03 -0300

    Ajustes nos testes

---
+++ src/Dongle.Js/Test/WRequiredIf/test.js
@@ -0,0 +1,12 @@
+﻿function runTests() {
+    test("Basic Tests", function ()
+    {
+        ok($('form').valid(), "must validate because conditional field is blank and either destiny field");
+
+        $('#check').val('1');
+        ok(!$('form').valid(), "must not validade because conditional field is filled with 1");
+
+        $('#title').val('alguma coisa');
+        ok($('form').valid(), "must validate because both fields are filled");
+    });
+}
