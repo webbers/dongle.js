de4dd7cb873efd7508524170b3bbc3748927867e
Afonso França <afonso.franca@gmail.com>
2012-09-24 16:57:27 -0300

    Ajustes Grunt com QuickBuild

--- bld/mainbuild.py
+++ bld/mainbuild.py
@@ -28,9 +28,9 @@ class MainBuild( Builder ):
     def build( self ):
         tempDir  = os.path.join( os.path.dirname( __file__ ), '../dist' )
         rootDir  = os.path.join( os.path.dirname( __file__ ), '..\\' )
-        gruntDir = os.environ['APPDATA'] + "\\npm\\";
+        #gruntDir = os.environ['APPDATA'] + "\\npm\\";
 
         self.addStep( DelTreeStep( tempDir ) )        
-        self.addStep( RunCommandStep( gruntDir + 'grunt.cmd deploy', rootDir ))
+        self.addStep( RunCommandStep( 'grunt.cmd deploy', rootDir ))
                 
         return Builder.build(self)
