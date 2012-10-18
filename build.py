9964a064e05346ce6bc7064cdcb1b04b986f9262
Afonso França <afonso.franca@gmail.com>
2012-10-18 18:22:51 -0300

    Revised to version 0.2.6 of PyHammer

--- build.py
+++ build.py
@@ -1,40 +1,44 @@
 import os
 import sys
 from pyhammer.builder import Builder
-from pyhammer.filters.csbinaryfilefilter import CsBinaryFileFilter
-from pyhammer.steps.io.copyfilteredfilesstep import CopyFilteredFilesStep
-from pyhammer.steps.io.deltreestep import DelTreeStep
-from pyhammer.steps.helpers.runcommandstep import RunCommandStep
-from pyhammer.steps.svn.svncommitdirstep import SvnCommitDirStep
-from pyhammer.steps.svn.svncreatetagdirstep import SvnCreateTagDirStep
-from pyhammer.steps.svn.svnimportdirstep import SvnImportDirStep
-from pyhammer.steps.svn.svndeletestep import SvnDeleteStep
-from pyhammer.steps.text.incrementversionstep import IncrementVersionStep
+from pyhammer.tasks.helpers.commandtask import CommandTask
+from pyhammer.tasks.io.copytask import CopyTask
+from pyhammer.tasks.io.deletetask import DeleteTask
+from pyhammer.tasks.svn.svncommittask import SvnCommitTask
+from pyhammer.tasks.svn.svncreatetagtask import SvnCreateTagTask
+from pyhammer.tasks.svn.svndeletetask import SvnDeleteTask
+from pyhammer.tasks.svn.svnimporttask import SvnImportTask
+from pyhammer.tasks.text.incrementversiontask import IncrementVersionTask
+
+svnUserName = ''
+if len(sys.argv)>2: svnUserName = sys.argv[2]
+svnPassword = ''
+if len(sys.argv)>3: svnPassword = sys.argv[3]
 
 #-Paths-----------------------------------------------------------------------------------------------------------------
-tempDir  = os.path.join( os.path.dirname( __file__ ), '../dist' )
-rootDir  = os.path.join( os.path.dirname( __file__ ), '../' )
-pubDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../pub' ) )
-versionFile = os.path.abspath( os.path.join( os.path.dirname( __file__ ), '../package.json' ) )
+tempDir  = os.path.join( os.path.dirname( __file__ ), 'dist' )
+rootDir  = os.path.join( os.path.dirname( __file__ ), '.' )
+pubDir = os.path.abspath( os.path.join( os.path.dirname( __file__ ), 'pub' ) )
+versionFile = os.path.abspath( os.path.join( os.path.dirname( __file__ ), 'package.json' ) )
 repoUrl = 'http://cronos:9090/gasrd/Web/pub/dongle.js/trunk'
 repoTagUrl = 'http://cronos:9090/gasrd/Web/pub/dongle.js/tags'
 
 #-Steps-----------------------------------------------------------------------------------------------------------------
-Builder.addStep( "del-temp", DelTreeStep( tempDir ) )
-Builder.addStep( "install-deps", RunCommandStep( 'npm.cmd install', rootDir ) )
-Builder.addStep( "grunt", RunCommandStep( 'grunt.cmd full', rootDir ) )
-Builder.addStep( "del-repo", SvnDeleteStep( repoUrl ) )
-Builder.addStep( "del-pub", DelTreeStep( pubDir ) )
-Builder.addStep( "copy", CopyFilteredFilesStep(CsBinaryFileFilter(), tempDir, pubDir ) )
-Builder.addStep( "import", SvnImportDirStep( pubDir, repoUrl ) )
-Builder.addStep( "increment-rev", IncrementVersionStep( versionFile, "revision", 3 ) )
-Builder.addStep( "increment-min", IncrementVersionStep( versionFile, "minor", 3 ) )
-Builder.addStep( "commit-version-file", SvnCommitDirStep( versionFile, 1, sys.argv[2], sys.argv[3]  ) )
-Builder.addStep( "create-tag", SvnCreateTagDirStep( repoUrl, repoTagUrl , versionFile ) )
+Builder.addTask( "del-temp", DeleteTask( tempDir ) )
+Builder.addTask( "install-deps", CommandTask( 'npm.cmd install', rootDir ) )
+Builder.addTask( "grunt", CommandTask( 'grunt.cmd full', rootDir ) )
+Builder.addTask( "del-repo", SvnDeleteTask( repoUrl ) )
+Builder.addTask( "del-pub", DeleteTask( pubDir ) )
+Builder.addTask( "copy", CopyTask(tempDir, pubDir ) )
+Builder.addTask( "import", SvnImportTask( pubDir, repoUrl ) )
+Builder.addTask( "increment-rev", IncrementVersionTask( versionFile, "revision", 3 ) )
+Builder.addTask( "increment-min", IncrementVersionTask( versionFile, "minor", 3 ) )
+Builder.addTask( "commit-version-file", SvnCommitTask( versionFile, 1, svnUserName, svnPassword  ) )
+Builder.addTask( "create-tag", SvnCreateTagTask( repoUrl, repoTagUrl , versionFile ) )
 
 #-Root steps------------------------------------------------------------------------------------------------------------
-Builder.addStep( "ps", "del-temp install-deps grunt")
-Builder.addStep( "ci", "ps del-repo del-pub copy import del-temp")
-Builder.addStep( "deploy-revision", "increment-rev ci create-tag commit-version-file")
-Builder.addStep( "deploy-minor", "increment-min ci create-tag commit-version-file")
+Builder.addTask( "ps", "del-temp install-deps grunt")
+Builder.addTask( "ci", "ps del-repo del-pub copy import del-temp")
+Builder.addTask( "deploy-revision", "increment-rev ci create-tag commit-version-file")
+Builder.addTask( "deploy-minor", "increment-min ci create-tag commit-version-file")
 Builder.runBuild()
