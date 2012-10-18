e88060287da2fdb184c34924ab57cacac6c71649
Renan Borges <renanborges02@gmail.com>
2012-10-18 16:54:09 -0300

    retirando um teste que não funcionou como devido

--- test/unit/winputfile.js
+++ test/unit/winputfile.js
@@ -3,10 +3,3 @@ test("Basic tests", function ()
 {
 	equal($('#winputfile').next().attr('class'), 'winputfilediv', 'Verifing if element was created');
 });
-
-test("Hover tests", function ()
-{
-	$('.winputfilediv').trigger('hover');	
-	ok($('.winputfile-panel-hover').length > 0, 'Creating hover effect');
-	
-});
