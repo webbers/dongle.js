afab788349b11840fc26eca32c1f594182420690
Renan <renanborges02@gmail.com>
2013-04-15 16:18:47 -0300

    fix build

--- build.py
+++ build.py
@@ -24,8 +24,8 @@ rootDir  = os.path.abspath( os.path.dirname( __file__ ) )
 tempDir  = os.path.join( rootDir, 'dist' )
 pubDir = os.path.join( rootDir, 'pub' )
 versionFile = os.path.join( rootDir, 'package.json' )
-repoUrl = 'http://cronos/svn/Web/pub/dongle.js/trunk'
-repoTagUrl = 'http://cronos/svn/Web/pub/dongle.js/tags'
+repoUrl = 'https://cronos/svn/Web/pub/dongle.js/trunk'
+repoTagUrl = 'https://cronos/svn/Web/pub/dongle.js/tags'
 
 #-Steps-----------------------------------------------------------------------------------------------------------------
 Builder.addTask( "del-temp", DeleteTask( tempDir ) )
